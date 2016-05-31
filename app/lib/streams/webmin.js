f441e37aae06543b97917582f865c1c804df75a6
cgeek <cem.moreau@gmail.com>
2016-05-31 11:25:59 +0200

    Fix: make the Windows version work again

--- app/lib/streams/webmin.js
+++ app/lib/streams/webmin.js
@@ -13,7 +13,7 @@ module.exports = function(dbConf, overConf, interfaces, httpLogs) {
 
   var webminCtrl = require('../../controllers/webmin.controller')(dbConf, overConf);
 
-  var fullPath = path.join(__dirname, '../../../ui/public/package');
+  var fullPath = path.join(__dirname, '../../../ui/package/public');
 
   let httpLayer = network.createServersAndListen('Duniter web admin', interfaces, httpLogs, fullPath, (app, httpMethods) => {
 
